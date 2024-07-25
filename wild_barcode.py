e9bf4869b92d98d61720ee24170fa198234841d4
HOR32 <161935933+HOR32@users.noreply.github.com>
2024-07-25 22:48:29 +0200

    Add files via upload

--- wild_barcode.py
+++ wild_barcode.py
@@ -1,21 +1,22 @@
 import keyboard
 import time
 import os
+import random
 def action(time_frame):
     start_time = time.time()
     while time.time() - start_time < time_frame:
         if keyboard.get_hotkey_name():
-            os.system("figlet -c you win")
+            os.system("figlet -c -f banner you win")
             return True
         time.sleep(0.01) 
-    os.system("figlet -c game over")
+    os.system("figlet -c -f banner game over")
     return False
 x = 3
 for i in range(3):
-    os.system(f"figlet -c {x}")
+    os.system(f"figlet -f banner -c {x}")
     x -= 1
     time.sleep(1)
     os.system("clear")
-os.system("figlet -c ACTION")
-timef = 0.5
+os.system("figlet -f banner -c ACTION")
+timef = random.uniform(0.5, 0.3)
 action(timef)
